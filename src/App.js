488eff0aba0345233b262837b3d2a54411bb3771
vara35 <kadiyalavaraprasad35@gmail.com>
2023-09-06 22:20:34 +0530

    added welcome component instead of react logo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import './App.css';
 import './index.css'
 
-import logo from './logo.svg';
+import Welcome from './Welcome'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='bg-blue'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 class="text-3xl font-bold underline text-red-500">
-    Hello world!
-  </h1>
-      </header>
+    <Welcome />
     </div>
   );
 }
